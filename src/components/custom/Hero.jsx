7154a6a2720abfaa1f6e9c3b9f3233d06a438164
Pranjal Snehi <pranjal.snehi@gmail.com>
2025-01-31 14:23:18 +0530

    Minor UI changes on Home Page

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -6,9 +6,14 @@ function Hero() {
     return (
         <div className='flex flex-col items-center px-4 sm:px-8 lg:px-56 gap-9 justify-center mb-16 sm:mb-24'>
             {/* Site Name */}
-            <div className='font-extrabold text-[30px] sm:text-[40px] md:text-[50px] text-center flex flex-col items-center justify-center mt-10'>
+            <div className='text-center flex flex-col items-center justify-center mt-10'>
                 <img src="/Logo.png" className='rounded-3xl h-40 w-40' />
-                TravelBuddy.ai
+                <p className='font-extrabold text-[30px] sm:text-[40px] md:text-[50px]'>
+                    TravelBuddy.ai
+                </p>
+                <p className='text-base font-semibold sm:text-lg lg:text-xl text-gray-400 text-center'>
+                    Plan Smarter, Travel Better.
+                </p>
             </div>
             {/* Hero Title and Description */}
             <h1 className='font-extrabold text-[30px] sm:text-[40px] md:text-[50px] text-center'>
