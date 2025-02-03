5d162c8b17e8ee69b95e4baf3c2a359b4e3281d0
Pranjal Snehi <pranjal.snehi@gmail.com>
2025-02-03 18:04:47 +0530

    UI changes

--- src/CreateTrip/index.jsx
+++ src/CreateTrip/index.jsx
@@ -20,6 +20,7 @@ import axios from 'axios';
 import { doc, setDoc } from 'firebase/firestore';
 import { db } from '@/services/firebaseConfig';
 import { useNavigate } from 'react-router';
+import { motion } from 'framer-motion';
 
 function CreateTrip() {
     const [place, setPlace] = useState();
@@ -115,127 +116,129 @@ function CreateTrip() {
     };
 
     return (
-        <div className="relative flex flex-col items-center px-4 sm:px-8 lg:px-56 gap-9 justify-center mb-16 sm:mb-24 
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }} className='w-full'>
+            <div className="relative flex flex-col items-center px-4 sm:px-8 lg:px-56 gap-9 justify-center mb-16 sm:mb-24 
     dark:bg-[url('/WorldMap.png')] bg-[url('/WorldMapLight.png')] bg-cover bg-center bg-no-repeat w-full min-h-screen">
-            {/* Blur Overlay */}
-            <div className="absolute inset-0 bg-white/30 dark:bg-black/30 backdrop-blur-sm"></div>
-            <div className='z-20 p-5 :'>
-                <h2 className='font-bold text-2xl sm:text-3xl text-[#32c1c1]'>Tell us your Travel Preferences</h2>
-                <p className='mt-3 dark:text-gray-300 text-gray-500 text- font-semibold sm:text-lg'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
-                <div className='mt-3 sm:mt-10 flex flex-col gap-8 sm:gap-10'>
+                {/* Blur Overlay */}
+                <div className="absolute inset-0 bg-white/30 dark:bg-black/30 backdrop-blur-sm"></div>
+                <div className='z-20 p-5 :'>
+                    <h2 className='font-bold text-2xl sm:text-3xl text-[#32c1c1]'>Tell us your Travel Preferences</h2>
+                    <p className='mt-3 dark:text-gray-300 text-gray-500 text- font-semibold sm:text-lg'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
+                    <div className='mt-3 sm:mt-10 flex flex-col gap-8 sm:gap-10'>
+                        <div className='mb-10 flex flex-col gap-2'>
+                            <h2 className='text-lg sm:text-xl font-medium my-3'>What is the destination of your choice?</h2>
+                            <GooglePlacesAutocomplete
+                                apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+                                selectProps={{
+                                    place,
+                                    onChange: (v) => {
+                                        setPlace(v);
+                                        handleInputChange('location', v);
+                                    },
+                                    styles: {
+                                        control: (provided, state) => ({
+                                            ...provided,
+                                            backgroundColor: darkMode ? '#000' : '#fff', // Black in dark mode, white in light mode
+                                            borderColor: state.isFocused ? '#32c1c1' : darkMode ? '#cbd5e1' : '#cbd5e1',
+                                            color: darkMode ? '#fff' : '#333',
+                                            boxShadow: state.isFocused ? '0 0 5px rgba(50, 193, 193, 0.5)' : 'none',
+                                            '&:hover': {
+                                                borderColor: '#32c1c1',
+                                            }
+                                        }),
+                                        input: (provided) => ({
+                                            ...provided,
+                                            color: darkMode ? '#fff' : '#333', // White text in dark mode
+                                        }),
+                                        singleValue: (provided) => ({
+                                            ...provided,
+                                            color: darkMode ? '#fff' : '#333', // Selected value text color
+                                        }),
+                                        menu: (provided) => ({
+                                            ...provided,
+                                            backgroundColor: darkMode ? '#000' : '#fff', // Dropdown background black in dark mode
+                                            borderRadius: '8px',
+                                            boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+                                        }),
+                                        option: (provided, state) => ({
+                                            ...provided,
+                                            backgroundColor: state.isSelected
+                                                ? '#32c1c1'
+                                                : state.isFocused
+                                                    ? darkMode ? '#333' : '#e0f7fa'
+                                                    : darkMode ? '#000' : '#fff',
+                                            color: state.isSelected ? '#fff' : darkMode ? '#fff' : '#333',
+                                            padding: '10px',
+                                            cursor: 'pointer',
+                                        }),
+                                    }
+                                }}
+                            />
+                        </div>
+                    </div>
                     <div className='mb-10 flex flex-col gap-2'>
-                        <h2 className='text-lg sm:text-xl font-medium my-3'>What is the destination of your choice?</h2>
-                        <GooglePlacesAutocomplete
-                            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
-                            selectProps={{
-                                place,
-                                onChange: (v) => {
-                                    setPlace(v);
-                                    handleInputChange('location', v);
-                                },
-                                styles: {
-                                    control: (provided, state) => ({
-                                        ...provided,
-                                        backgroundColor: darkMode ? '#000' : '#fff', // Black in dark mode, white in light mode
-                                        borderColor: state.isFocused ? '#32c1c1' : darkMode ? '#cbd5e1' : '#cbd5e1',
-                                        color: darkMode ? '#fff' : '#333',
-                                        boxShadow: state.isFocused ? '0 0 5px rgba(50, 193, 193, 0.5)' : 'none',
-                                        '&:hover': {
-                                            borderColor: '#32c1c1',
-                                        }
-                                    }),
-                                    input: (provided) => ({
-                                        ...provided,
-                                        color: darkMode ? '#fff' : '#333', // White text in dark mode
-                                    }),
-                                    singleValue: (provided) => ({
-                                        ...provided,
-                                        color: darkMode ? '#fff' : '#333', // Selected value text color
-                                    }),
-                                    menu: (provided) => ({
-                                        ...provided,
-                                        backgroundColor: darkMode ? '#000' : '#fff', // Dropdown background black in dark mode
-                                        borderRadius: '8px',
-                                        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-                                    }),
-                                    option: (provided, state) => ({
-                                        ...provided,
-                                        backgroundColor: state.isSelected
-                                            ? '#32c1c1'
-                                            : state.isFocused
-                                                ? darkMode ? '#333' : '#e0f7fa'
-                                                : darkMode ? '#000' : '#fff',
-                                        color: state.isSelected ? '#fff' : darkMode ? '#fff' : '#333',
-                                        padding: '10px',
-                                        cursor: 'pointer',
-                                    }),
-                                }
-                            }}
+                        <h2 className='text-lg sm:text-xl font-medium my-3'>How many days do you want to travel?</h2>
+                        <Input placeholder={'Ex. 3'} type="number"
+                            onChange={(e) => handleInputChange('noOfDays', e.target.value)}
+                            className='w-full sm:w-1/2 border-slate-300'
                         />
                     </div>
-                </div>
-                <div className='mb-10 flex flex-col gap-2'>
-                    <h2 className='text-lg sm:text-xl font-medium my-3'>How many days do you want to travel?</h2>
-                    <Input placeholder={'Ex. 3'} type="number"
-                        onChange={(e) => handleInputChange('noOfDays', e.target.value)}
-                        className='w-full sm:w-1/2 border-slate-300'
-                    />
-                </div>
-                <div className='mb-10 flex flex-col gap-2'>
-                    <h2 className='text-lg sm:text-xl font-medium my-3'>What is your budget?</h2>
-                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5'>
-                        {SelectBudgetOptions.map((item, index) => (
-                            <div key={index}
-                                onClick={() => handleInputChange('budget', item.title)}
-                                className={`p-4 border backdrop-blur-md border-slate-300 shadow-lg rounded-lg hover:shadow-lg hover:border-[#32c1c1] cursor-pointer ${formData?.budget == item.title && 'hover:shadow-[#32c1c179] shadow-[#32c1c179] backdrop-blur-3xl '}`}>
-                                <h2 className='text-3xl sm:text-4xl'>{item.icon}</h2>
-                                <h2 className='font-bold text-lg'>{item.title}</h2>
-                                <h2 className='text-sm text-gray-500 dark:text-gray-300'>{item.desc}</h2>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <div className='mb-10 flex flex-col gap-2'>
-                    <h2 className='text-lg sm:text-xl font-medium my-3'>Who do you plan on traveling with on your next adventure?</h2>
-                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5'>
-                        {SelectNoOfPersons.map((item, index) => (
-                            <div key={index}
-                                onClick={() => handleInputChange('noOfPeople', item.no)}
-                                className={`p-4 border backdrop-blur-xl border-slate-300 shadow-lg rounded-lg hover:shadow-lg hover:border-[#32c1c1] cursor-pointer ${formData?.noOfPeople == item.no && 'hover:shadow-[#32c1c179] shadow-[#32c1c179] backdrop-blur-3xl'}`}>
-                                <h2 className='text-3xl sm:text-4xl'>{item.icon}</h2>
-                                <h2 className='font-bold text-lg'>{item.title}</h2>
-                                <h2 className='text-sm text-gray-500 dark:text-gray-300'>{item.desc}</h2>
-                                <h2 className='text-sm text-gray-500 dark:text-gray-300'>{item.no}</h2>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <div className="mt-5 mb-20 flex items-center justify-end">
-                    <Button onClick={onGenerateTrip}
-                        disabled={loading}
-                        className='w-full sm:w-auto'>
-                        {loading ? <AiOutlineLoading3Quarters className='h-6 sm:h-7 w-6 sm:w-7 animate-spin font-bold' /> : <p>Generate Trip</p>}
-                    </Button>
-                </div>
-                <Dialog open={openDialogue}>
-                    <DialogContent>
-                        <div className='flex flex-row items-center'>
-                            <img src="/Logo.png" alt="" className='h-10 rounded-3xl' />
-                            <div className='pl-2 font-semibold text-xl text-center'>
-                                TravelBuddy.ai
-                            </div>
+                    <div className='mb-10 flex flex-col gap-2'>
+                        <h2 className='text-lg sm:text-xl font-medium my-3'>What is your budget?</h2>
+                        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5'>
+                            {SelectBudgetOptions.map((item, index) => (
+                                <div key={index}
+                                    onClick={() => handleInputChange('budget', item.title)}
+                                    className={`p-4 border backdrop-blur-md border-slate-300 shadow-lg rounded-lg hover:shadow-lg hover:border-[#32c1c1] cursor-pointer ${formData?.budget == item.title && ' backdrop-blur-3xl bg-black/20 dark:bg-white/20'}`}>
+                                    <h2 className='text-3xl sm:text-4xl'>{item.icon}</h2>
+                                    <h2 className='font-bold text-lg'>{item.title}</h2>
+                                    <h2 className='text-sm text-gray-500 dark:text-gray-300'>{item.desc}</h2>
+                                </div>
+                            ))}
                         </div>
-                        <DialogTitle>Sign In with Google</DialogTitle>
-                        <DialogDescription>
-                            Sign in to the app with Google authentication securely.
-                        </DialogDescription>
-                        <Button onClick={LogIn} className='w-full flex flex-row gap-2'>
-                            Sign in with Google <FcGoogle className='h-7 w-7' />
+                    </div>
+                    <div className='mb-10 flex flex-col gap-2'>
+                        <h2 className='text-lg sm:text-xl font-medium my-3'>Who do you plan on traveling with on your next adventure?</h2>
+                        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5'>
+                            {SelectNoOfPersons.map((item, index) => (
+                                <div key={index}
+                                    onClick={() => handleInputChange('noOfPeople', item.no)}
+                                    className={`p-4 border backdrop-blur-xl border-slate-300 shadow-lg rounded-lg hover:shadow-lg hover:border-[#32c1c1] cursor-pointer ${formData?.noOfPeople == item.no && 'backdrop-blur-xl bg-black/20 dark:bg-white/20'}`}>
+                                    <h2 className='text-3xl sm:text-4xl'>{item.icon}</h2>
+                                    <h2 className='font-bold text-lg'>{item.title}</h2>
+                                    <h2 className='text-sm text-gray-500 dark:text-gray-300'>{item.desc}</h2>
+                                    <h2 className='text-sm text-gray-500 dark:text-gray-300'>{item.no}</h2>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="mt-5 mb-20 flex items-center justify-end">
+                        <Button onClick={onGenerateTrip}
+                            disabled={loading}
+                            className='w-full sm:w-auto'>
+                            {loading ? <AiOutlineLoading3Quarters className='h-6 sm:h-7 w-6 sm:w-7 animate-spin font-bold' /> : <p>Generate Trip</p>}
                         </Button>
-                    </DialogContent>
-                </Dialog>
+                    </div>
+                    <Dialog open={openDialogue}>
+                        <DialogContent>
+                            <div className='flex flex-row items-center'>
+                                <img src="/Logo.png" alt="" className='h-10 rounded-3xl' />
+                                <div className='pl-2 font-semibold text-xl text-center'>
+                                    TravelBuddy.ai
+                                </div>
+                            </div>
+                            <DialogTitle>Sign In with Google</DialogTitle>
+                            <DialogDescription>
+                                Sign in to the app with Google authentication securely.
+                            </DialogDescription>
+                            <Button onClick={LogIn} className='w-full flex flex-row gap-2'>
+                                Sign in with Google <FcGoogle className='h-7 w-7' />
+                            </Button>
+                        </DialogContent>
+                    </Dialog>
+                </div>
             </div>
-        </div>
+        </motion.div>
     );
 }
 
--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -2,56 +2,64 @@ import React from 'react'
 import { Button } from '../ui/button'
 import { Link } from 'react-router'
 import RotatingText from '@/jsrepo/TextAnimations/RotatingText/RotatingText'
+import { motion } from 'framer-motion'
 
 
 function Hero() {
     return (
-        <div
-            className="relative flex flex-col items-center px-4 sm:px-8 lg:px-56 gap-9 justify-center mb-16 sm:mb-24 
-    dark:bg-[url('/WorldMap2.png')] bg-[url('/WorldMapLight.png')] bg-cover bg-center bg-no-repeat w-full h-full"
+        <motion.div
+            initial={{ opacity: 0, y: 50 }} // Start 50px below and invisible
+            animate={{ opacity: 1, y: 0 }}  // Move to normal position and fade in
+            transition={{ duration: 0.5, ease: "easeOut" }} // Smooth animation
+            className='w-full'
         >
-            {/* Blur Overlay */}
-            <div className="absolute inset-0 bg-white/30 dark:bg-black/30 backdrop-blur-sm dark:backdrop-blur-none"></div>
+            <div
+                className="relative flex flex-col items-center px-4 sm:px-8 lg:px-56 gap-9 justify-center mb-16 sm:mb-24 
+    dark:bg-[url('/WorldMap2.png')] bg-[url('/WorldMapLight.png')] bg-cover bg-center bg-no-repeat w-full h-full"
+            >
+                {/* Blur Overlay */}
+                <div className="absolute inset-0 bg-white/30 dark:bg-black/30 backdrop-blur-sm dark:backdrop-blur-none"></div>
 
-            {/* Content */}
-            <div className="relative z-10 text-center flex flex-col items-center justify-center mt-10">
-                <img src="/Logo.png" className='rounded-3xl h-40 w-40' />
-                <p className='font-extrabold text-[30px] sm:text-[40px] md:text-[50px]'>
-                    TravelBuddy.ai
-                </p>
-                <div className='flex items-center justify-center gap-1'>
-                    <p className='text-2xl font-semibold sm:text-2xl lg:text-2xl text-gray-400 text-center'>
-                        Plan
+                {/* Content */}
+                <div className="relative z-10 text-center flex flex-col items-center justify-center mt-10">
+                    <img src="/Logo.png" className='rounded-3xl h-40 w-40' />
+                    <p className='font-extrabold text-[30px] sm:text-[40px] md:text-[50px]'>
+                        TravelBuddy.ai
                     </p>
-                    <RotatingText
-                        texts={['Smarter!', 'Better!']}
-                        mainClassName="p-1 text-2xl font-semibold sm:text-2xl lg:text-2xl text-gray-400 overflow-hidden justify-center rounded-lg"
-                        staggerFrom={"last"}
-                        initial={{ y: "100%" }}
-                        animate={{ y: 0 }}
-                        exit={{ y: "-120%" }}
-                        staggerDuration={0.025}
-                        splitLevelClassName="overflow-hidden"
-                        transition={{ type: "spring", damping: 30, stiffness: 400 }}
-                        rotationInterval={2000}
-                    />
+                    <div className='flex items-center justify-center gap-1'>
+                        <p className='text-2xl font-semibold sm:text-2xl lg:text-2xl text-gray-400 text-center'>
+                            Plan
+                        </p>
+                        <RotatingText
+                            texts={['Smarter!', 'Better!']}
+                            mainClassName="p-1 text-2xl font-semibold sm:text-2xl lg:text-2xl text-gray-400 overflow-hidden justify-center rounded-lg"
+                            staggerFrom={"last"}
+                            initial={{ y: "100%" }}
+                            animate={{ y: 0 }}
+                            exit={{ y: "-120%" }}
+                            staggerDuration={0.025}
+                            splitLevelClassName="overflow-hidden"
+                            transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                            rotationInterval={2000}
+                        />
+                    </div>
                 </div>
-            </div>
 
-            {/* Hero Title and Description */}
-            <h1 className='relative z-10 font-extrabold text-[30px] sm:text-[40px] md:text-[50px] text-center'>
-                <span className='text-[#32c1c1]'>Discover Your Next Adventure with AI:</span>
-                <br />
-                Personalized Itineraries at Your Fingertips
-            </h1>
-            <p className='relative z-10 text-base sm:text-lg lg:text-xl text-gray-500 text-center'>
-                Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.
-            </p>
-            <img src="/HeroPage.png" alt="" className='w-full h-[450px] sm:h-[400px] object-contain z-10' />
-            <Link to={'/CreateTrip'}>
-                <Button className='relative z-10 mt-6 px-6 py-3 sm:px-8 sm:py-4 bg-[#32c1c1] mb-10'>Get Started</Button>
-            </Link>
-        </div>
+                {/* Hero Title and Description */}
+                <h1 className='relative z-10 font-extrabold text-[30px] sm:text-[40px] md:text-[50px] text-center'>
+                    <span className='text-[#32c1c1]'>Discover Your Next Adventure with AI:</span>
+                    <br />
+                    Personalized Itineraries at Your Fingertips
+                </h1>
+                <p className='relative z-10 text-base sm:text-lg lg:text-xl text-gray-500 text-center'>
+                    Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.
+                </p>
+                <img src="/HeroPage.png" alt="" className='w-full h-[450px] sm:h-[400px] object-contain z-10' />
+                <Link to={'/CreateTrip'}>
+                    <Button className='relative z-10 mt-6 px-6 py-3 sm:px-8 sm:py-4 bg-[#32c1c1] mb-10'>Get Started</Button>
+                </Link>
+            </div>
+        </motion.div>
     )
 }
 
