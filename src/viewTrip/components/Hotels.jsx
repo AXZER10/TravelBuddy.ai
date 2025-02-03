ed759ee25b060a8d3d1600797a64e6aba9173774
Pranjal Snehi <pranjal.snehi@gmail.com>
2025-02-03 22:03:14 +0530

    Minor fixes in optimisation for smaller screen sizes

--- src/viewTrip/components/Hotels.jsx
+++ src/viewTrip/components/Hotels.jsx
@@ -8,7 +8,7 @@ function Hotels({ trip }) {
     return (
         <div className='p-5 bg-gray-200 mt-5 rounded-xl dark:bg-gray-500'>
             <h2 className='font-bold text-xl sm:text-2xl dark:text-black'>Hotel Recommendations</h2>
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5 mt-4'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-10 mt-4 p-5'>
                 {trip?.tripData?.hotelOptions?.map((hotel, index) => (
                     <Link 
                         key={index} 
